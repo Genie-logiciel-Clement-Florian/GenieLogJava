b7fefd5a64d31eb2a0cd8ebaa4c802af1e07c51c
Florian Moussé <florian.mousse@etud.univ-angers.fr>
2023-01-16 22:57:22 +0100

    Ajustement des tests unitaires + suppression des tests inutiles

---
+++ src/tests/TestsSuiteRunner.java
@@ -0,0 +1,17 @@
+package tests;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestsSuiteRunner {
+
+	public static void main(String[] args) {
+
+		Result result = JUnitCore.runClasses(TestsSuite.class);
+		
+		for (Failure fail : result.getFailures()) System.out.println(fail.toString());
+		
+		if (result.wasSuccessful()) System.out.println("All tests finished successfully...");
+	}
+}
