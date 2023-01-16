0e7e7b6fbd928204595d35b6652e8917a1fe563c
Florian Moussé <florian.mousse@etud.univ-angers.fr>
2023-01-17 00:37:54 +0100

    Documentation de la fonction deposer

--- src/myPackage/DossierBancaire.java
+++ src/myPackage/DossierBancaire.java
@@ -10,6 +10,7 @@ public class DossierBancaire {
 		epargne = new Epargne(0);
 	}
 
+	// Dépôt de 40% du solde sur le compte Courant et 60% sur le compte Epargne
 	public void deposer(double value) {
 		courant.setSolde(value * 0.4);
 		epargne.setSolde(value * 0.6);
